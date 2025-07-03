b82a17701a03d3d5ec8871350e3a50fab247d1b1
hariomphogat <hariom9728034346@gmail.com>
2025-07-04 00:53:18 +0530

    fix:navbar typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import Hero from "./sections/Hero";
 import Showcase from "./sections/Showcase";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import LogoSection from "./sections/LogoSection";
 import FeatureCards from "./sections/FeatureCards";
 import Education from "./sections/Education";
 import TechStack from "./sections/TechStack";
 import Contact from "./sections/Contact";
-import StartupLoader from "./components/StartupLoader";
 import Footer from "./sections/Footer";
 
 const App = () => {
   return (
     <>
-      {/* <StartupLoader /> */}
       <Navbar />
       <Hero />
       <Showcase />
